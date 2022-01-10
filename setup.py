63402b8b0a151609540a4d40bfdb4e2ca9c844c4
Andrew Kondrich <andrewk1@stanford.edu>
2022-01-09 21:57:36 -0800

    install requires

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name="cands",
       version="0.0.1",
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=[
+        "torch>=1.6.0",
+        "tqdm>=4.27",
+        "numpy"
+      ],
       )
